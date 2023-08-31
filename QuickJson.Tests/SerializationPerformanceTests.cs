707cfbba47ccb2b4bd7168623c1eed8394291690
gintarasoe <gintaras@eldorado.gg>
2023-08-31 22:28:06 +0300

    Perf checks - 10x slower...

---
+++ QuickJson.Tests/SerializationPerformanceTests.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace QuickJson.Tests;
+
+public class SerializationPerformanceTests
+{
+    private const int NumberOfTestingIterations = 5000;
+
+    [Fact]
+    public void Serialize_WithCustomPaths_PerformsWell()
+    {
+        // Arrange
+        var blog = Helpers.CreateTestBlog();
+
+        // Act
+        var sw = Stopwatch.StartNew();
+        for (var i = 0; i < NumberOfTestingIterations; i++)
+        {
+            QuickJson.SerializeObject(blog);
+        }
+        sw.Stop();
+
+        // Assert
+        Assert.Fail($"Paths Results: {sw.ElapsedMilliseconds / NumberOfTestingIterations}ms, {sw.ElapsedTicks / NumberOfTestingIterations}ticks");
+    }
+
+    [Fact]
+    public void Serialize_WithoutCustomPaths_PerformsWell()
+    {
+        // Arrange
+        var blog = Helpers.CreateTestBlogWithoutAttributes();
+
+        // Act
+        var sw = Stopwatch.StartNew();
+        for (var i = 0; i < NumberOfTestingIterations; i++)
+        {
+            QuickJson.SerializeObject(blog);
+        }
+        sw.Stop();
+
+        // Assert
+        Assert.Fail($"NoPaths Results: {sw.ElapsedMilliseconds / NumberOfTestingIterations}ms, {sw.ElapsedTicks / NumberOfTestingIterations}ticks");
+    }
+
+    [Fact]
+    public void Serialize_WithNewtonsoft_Baseline()
+    {
+        // Arrange
+        var blog = Helpers.CreateTestBlog();
+
+        // Act
+        var sw = Stopwatch.StartNew();
+        for (var i = 0; i < NumberOfTestingIterations; i++)
+        {
+            JsonConvert.SerializeObject(blog);
+        }
+        sw.Stop();
+
+        // Assert
+        Assert.Fail($"Newtonsoft Results: {sw.ElapsedMilliseconds / NumberOfTestingIterations}ms, {sw.ElapsedTicks / NumberOfTestingIterations}ticks");
+    }
+}
